6d4045c4927d82f480e66760f1d85e13ad5cff94
zisoo <zisoo_3@naver.com>
2023-06-22 13:15:53 +0900

    fix: [최지수] 콘솔에 나타나는 reactDOM 경고 해결

--- mfe/container-app/src/bootstrap.js
+++ mfe/container-app/src/bootstrap.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import App from './App.jsx';
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+// react 18에서는 아래와 같은 메서드 형태가 지원되지 않는다.
+// ReactDOM.render(<App/>, document.getElementById('root'))
+// 아래 코드와 위의 코드는 같은 기능을 한다.
+
+const rootElement = document.getElementById('root');
+createRoot(rootElement).render(<App/>);
