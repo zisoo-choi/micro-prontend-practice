be8445640fa0e411bbd7ae0c66fcf7d5a76b975e
zisoo <zisoo_3@naver.com>
2023-06-22 12:59:46 +0900

    feat: [최지수] vue-module-app 화면 띄우기 [MFEx]

---
+++ mfe/vue-module-app/src/components/Sample.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(0)
+</script>
+
+<template>
+    <div id="moduleApp">
+        <p>Vue - Module App</p>
+        <button @click="count++">
+            빨리 버튼 눌러봐 {{ count }} 번 눌렀어
+        </button>
+    </div>
+</template>
+
+<style scoped>
+#moduleApp{
+    background-color: yellow;
+}
+</style>
