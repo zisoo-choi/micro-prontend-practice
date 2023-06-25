6d7e897d3afa8fbb4da4601a93d4b36b37821f08
zisoo <zisoo_3@naver.com>
2023-06-25 23:30:12 +0900

    feat: [최지수] container-app 주석 다는 중 [ANNOTATIONx]

--- mfe/container-app/src/App.jsx
+++ mfe/container-app/src/App.jsx
@@ -1,19 +1,31 @@
 import React, { useEffect, useRef } from "react";
 import { mount } from 'vueModuleApp/Sample';
 
+// App 함수
 const App = () => {
   const ref = useRef(null)
 
+  // useEffect를 사용하여 컴포넌트가 마운트되었을 때 한 번 실행되는 효과를 정의한다.
   useEffect(() => {
+    /*
+      mount 함수를 호출하여
+      vue 마이크로 프론트엔드 애플리케이션을
+      ref.currend에 저장된 DOM 요소에 마운트합니다.
+      이를 통해 vue 애플리케이션을 react 애플리케이션에 통합합니다.
+    */
     mount(ref.current);
+    // 빈 배열 []을 두 번째 인자로 전달하여 의존성이 없음을 나타낸다.
   }, []);
 
   return (
+    // react 애플리케이션의 root 요소
     <div style=
         {{ margin: '10px', padding: '10px', textAlign: 'center', backgroundColor: 'cyan'}}>
       <h1>React - container-app</h1>
-
-      {/* 아래 코드가 뷰 컴포넌트 */}
+       
+      {/*
+        아래 코드가 뷰 컴포넌트 (vue 컴포넌트를 포함한 JSX라고 한다.)
+      */}
       <div ref={ref} />
     </div>
   )
--- mfe/container-app/src/bootstrap.js
+++ mfe/container-app/src/bootstrap.js
@@ -8,5 +8,14 @@ import App from './App.jsx';
 // ReactDOM.render(<App/>, document.getElementById('root'))
 // 아래 코드와 위의 코드는 같은 기능을 한다.
 
+/*
+    HTML에서 id가 root인 요소를 찾아 rootElement 변수에 할당한다.
+    (이 요소는 React 애플리케이션이 렌더링될 대상 요소이다.)
+*/
 const rootElement = document.getElementById('root');
+/*
+    createRoot 메서드를 사용하여 rootElement를 루트로 설정하고,
+    render 메서드를 호출하여 <APP/> 컴포넌트를 해당 루트에 렌더링한다.
+    이를 통해 React 애플리케이션이 시작되고 화면이 표시된다.
+*/
 createRoot(rootElement).render(<App/>);
